3929ba72c260fcd6bd889046c6a8214e7d7fe545
Luc Yriarte <lyriarte@gmail.com>
2022-02-26 23:22:25 +0100

    Phase chord progressions should be rings also

--- loops/bridge phases exploration.rb
+++ loops/bridge phases exploration.rb
@@ -8,12 +8,12 @@ set :sub_beat, 4
 base_chords = [
   (chord :D, :minor7),  (chord :D, :minor7),
   (chord :E, :minor7),  (chord :E, :minor7)
-]
+].ring
 
 bridge_chords = [
   (chord :Fs, :minor7),  (chord :E, :minor7),
   (chord :B3, :minor7),  (chord :B3, :minor7)
-]
+].ring
 
 set :phases, [base_chords, bridge_chords]
 set :chords, (get :phases)[0]
