ea39f59cc25b9123684153d905971dfc5ea23d5b
Luc Yriarte <lyriarte@gmail.com>
2022-03-12 14:57:26 +0100

    Keyboards synth block refactor

--- live_kit/instruments/keyboards.rb
+++ live_kit/instruments/keyboards.rb
@@ -15,79 +15,85 @@ end
 
 # one chord per beat
 define :keys_chords_beat do | ch, am=1, sy=:beep |
-  use_synth sy
-  play ch, amp: am, release: 0.8
+  with_synth sy do
+    play ch, amp: am, release: 0.8
+  end
 end
 
 # short chord odd sub beat
 define :keys_chords_odd_sub_beat do | ch, am=1, sy=:beep |
-  use_synth sy
-  sleep 0.5
-  play ch, amp: am, release: 0.1
+  with_synth sy do
+    sleep 0.5
+    play ch, amp: am, release: 0.1
+  end
 end
 
 # short double sub random
 define :keys_rnd_dbl_sub_beat do | ch, sb=4, am=1, sy=:beep |
-  use_synth sy
-  play ch.choose, release: 0.05
-  (2*sb - 1).times do
-    sleep 1/Float(2*sb)
-    play ch.choose, release:0.05, amp: am
+  with_synth sy do
+    play ch.choose, release: 0.05
+    (2*sb - 1).times do
+      sleep 1/Float(2*sb)
+      play ch.choose, release:0.05, amp: am
+    end
   end
 end
 
 # short double sub slide
 define :keys_rnd_sub_slide do | ch, sb=4, am=1, sy=:beep |
-  use_synth sy
-  n0 = ch.choose()
-  (sb - 1).times do
-    n1 = ch.choose
-    p0 = play n0, note_slide: 1/Float(sb), release: 1/Float(sb), amp: am
-    control p0, note: n1
-    sleep 1/Float(sb)
-    n0 = n1
+  with_synth sy do
+    n0 = ch.choose()
+    (sb - 1).times do
+      n1 = ch.choose
+      p0 = play n0, note_slide: 1/Float(sb), release: 1/Float(sb), amp: am
+      control p0, note: n1
+      sleep 1/Float(sb)
+      n0 = n1
+    end
+    play n0, release: 1/Float(sb)
   end
-  play n0, release: 1/Float(sb)
 end
 
 # ---- sync :bar
 
 # one chord per bar
 define :keys_chords_bar do | ch, nb=4, am=1, sy=:beep |
-  use_synth sy
-  play ch, amp: am, release: nb
+  with_synth sy do
+    play ch, amp: am, release: nb
+  end
 end
 
 # slide within random notes in the current chord
 define :bar_slide do | ch, nb=4, am=1, sy=:beep |
-  use_synth sy
-  n0 = ch.choose()
-  (nb - 1).times do
-    n1 = ch.choose
-    p0 = play n0, note_slide: 1
-    control p0, note: n1
-    sleep 1
-    n0 = n1
+  with_synth sy do
+    n0 = ch.choose()
+    (nb - 1).times do
+      n1 = ch.choose
+      p0 = play n0, note_slide: 1
+      control p0, note: n1
+      sleep 1
+      n0 = n1
+    end
+    play n0
   end
-  play n0
-  #sleep 1
 end
 
 # latin clave on 2 bars
 define :keys_latin_2_bar_4 do | ch, br, am=1, sy=:fm |
-  use_synth sy
-  if br % 2 == 0 then
-    play ch, amp: am, release: 0.2
-    sleep 1
-    play ch, amp: am, release: 0.2
-    sleep 1.5
-    play ch, amp: am, release: 0.2
-    sleep 1
-    play ch, amp: am, release: 0.2
-  else
-    sleep 0.5
-    play ch, amp: am, release: 0.2
-    sleep 1
-    play ch, amp: am, release: 0.2
+  with_synth sy do
+    if br % 2 == 0 then
+      play ch, amp: am, release: 0.2
+      sleep 1
+      play ch, amp: am, release: 0.2
+      sleep 1.5
+      play ch, amp: am, release: 0.2
+      sleep 1
+      play ch, amp: am, release: 0.2
+    else
+      sleep 0.5
+      play ch, amp: am, release: 0.2
+      sleep 1
+      play ch, amp: am, release: 0.2
+    end
   end
 end
