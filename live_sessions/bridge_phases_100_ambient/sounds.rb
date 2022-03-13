3850a31378eb4a1d2fbfd74b2427919c10aa1a5a
Luc Yriarte <lyriarte@gmail.com>
2022-03-13 23:29:13 +0100

    Ambient sounds and pentatonic bells

--- live_sessions/bridge_phases_100_ambient/sounds.rb
+++ live_sessions/bridge_phases_100_ambient/sounds.rb
@@ -6,19 +6,18 @@ use_bpm get(:bpm)
 live_loop :keys do
 #  bt, ch = (sync :beat)
 #  keys_chords_odd_sub_beat ch, am=0.5
-#  keys_rnd_dbl_sub_beat ch, (get :sub_beat) if one_in (get :beats)
+#  keys_rnd_dbl_sub_beat ch, (get :sub_beat) if one_in 2
   br, ch = (sync :bar)
   play ch, amp: 0.5
-#  with_synth :dark_ambience do
-#    play ch, amp: 0.8, attack: 0.5, decay: 0.25, sustain: (get :beats), release: (get :beats)
-#    play (rrand ch[0], ch[0]+12, step: 1), amp: 2, attack: 0.5, decay: 0.25, sustain: (get :beats), release: (get :beats)
-#  end
+#  keys_rnd_penta_bells ch, am=0.5
 #  keys_rnd_dbl_sub_beat ch, (get :sub_beat)
 end
 
 live_loop :sounds do
-  bt, ch = (sync :beat)
+#  bt, ch = (sync :beat)
 #  sample :ambi_haunted_hum, rate: (rrand -2, 2), beat_stretch: (rrand 1, (get :beats) * 2) 
+  br, ch = (sync :bar)
+#  keys_dark_ambi_rnd_chord ch
 end
 
 
@@ -40,6 +39,34 @@ define :keys_rnd_dbl_sub_beat do | ch, sb=4, am=1, sy=:beep |
       sleep 1/Float(2*sb)
       play ch.choose, release:0.05, amp: am
     end
+  end
+end
+
+# random pentatonic bells
+define :keys_rnd_penta_bells do | ch, am=1 |
+  if one_in(3)
+      sy = :pretty_bell
+  else
+      sy = :dull_bell
+  end
+  with_synth sy do
+    i=1
+    while i>0.05 do 
+      j = rrand(0, i, step: 0.05)
+        play (scale ch[0], :minor_pentatonic).pick
+        sleep(j)
+        i=i-j
+    end
+  end
+end
+
+# ---- ---- ---- ---- ambient
+
+# dark ambience base chord plus random note
+define :keys_dark_ambi_rnd_chord do | ch, am=1, sy=:dark_ambience |
+  with_synth sy do
+    play ch, amp: am, attack: 0.5, decay: 0.25, release: (get :beats)*2
+    play (rrand ch[0], ch[0]+12, step: 1), amp: am, attack: 2, decay: 0.25, release: (get :beats)*1.5
   end
 end
 
