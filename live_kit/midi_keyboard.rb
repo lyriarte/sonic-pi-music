8afdd22e94c8acc2d431b0d9e88573a4fc489857
Luc Yriarte <lyriarte@gmail.com>
2022-03-19 20:15:35 +0100

    Real time support for midi synth

--- live_kit/midi_keyboard.rb
+++ live_kit/midi_keyboard.rb
@@ -1,10 +1,8 @@
 #  ---- ----  ----  ---- midi keyboard input
 
-# ---- midi chord
 
-set :midi_chrd, []
-set :midi_hold, false
-# default to beep synth
+# ---- midi synth default to beep synth
+
 set :midi_synth, :beep
 set :midi_amp, 1
 set :midi_attack, 0
@@ -13,31 +11,43 @@ set :midi_sustain, 0
 set :midi_release, 1
 
 
+# ---- midi chord
 
-live_loop :play_midi_chord do
+set :midi_chrd, []
+set :midi_hold, false
+
+define :play_midi_chord do | ch |
+  play ch, amp: (get :midi_amp), 
+    attack: (get :midi_attack), 
+    decay: (get :midi_decay), 
+    sustain: (get :midi_sustain), 
+    release: (get :midi_release)
+end
+
+live_loop :midi_chord_loop do
+  use_real_time
   use_synth (get :midi_synth)
   ch, on = sync :midi_changed
-  if on or (get :midi_hold)
-    play ch, amp: (get :midi_amp), 
-      attack: (get :midi_attack), 
-      decay: (get :midi_decay), 
-      sustain: (get :midi_sustain), 
-      release: (get :midi_release)
-  end
+  play_midi_chord ch if (get :midi_hold)
 end
 
 
 # ---- midi event capture
 
 live_loop :midi_note_on do
+  use_real_time
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_on"
   ch = (get :midi_chrd).dup
   ch.append(nt)
+  play_midi_chord ch
   set :midi_chrd, ch
-  cue :midi_changed, ch, true
+  if (get :midi_hold)
+    cue :midi_changed, ch, true
+  end
 end
 
 live_loop :midi_note_off do
+  use_real_time
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_off"
   ch = (get :midi_chrd).dup
   ch.delete(nt)
