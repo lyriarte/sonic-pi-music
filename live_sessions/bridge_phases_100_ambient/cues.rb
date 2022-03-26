83b6c35db65e900a3f710730d01d249b2f3a862d
Luc Yriarte <lyriarte@gmail.com>
2022-03-26 13:32:43 +0100

    set play flags from cues loop

--- live_sessions/bridge_phases_100_ambient/cues.rb
+++ live_sessions/bridge_phases_100_ambient/cues.rb
@@ -8,5 +8,31 @@ cue :start
 
 live_loop :cues do
   br, ch = (sync :bar)
-  cue :phase, 1 if (get :n_bar) >= 64 
+  
+  set :play_bass_octave, nil
+  set :play_bass_octave, true if (get :n_bar).between?(16,31)
+  set :play_bass_base, nil
+  set :play_bass_base, true if (get :n_bar) < 16 or (get :n_bar) >= 64
+  set :play_cymbal_beat, nil
+  set :play_cymbal_beat, true if (get :n_bar) < 24
+  set :play_cymbal_sub, nil
+  set :play_cymbal_sub, true if (get :n_bar) >= 24
+  set :play_drums_house, nil
+  set :play_drums_house, true if (get :n_bar) >= 28
+  set :play_chords_odd, nil
+  set :play_chords_odd, true if (get :n_bar).between?(20,63)
+  set :play_chords_rnd, nil
+  set :play_chords_rnd, true if (get :n_bar).between?(48,63) and one_in 2
+  set :play_chords_beat, nil
+  set :play_chords_beat, true if (get :n_bar) < 32
+  set :play_bells_rnd, nil
+  set :play_bells_rnd, true if (get :n_bar).between?(72,95)
+  set :play_chords_rnd_beat, nil
+  set :play_chords_rnd_beat, true if (get :n_bar).between?(16,31)
+  set :play_haunted, nil
+  set :play_haunted, true if (get :n_bar) >= 84
+  set :play_ambient, nil
+  set :play_ambient, true if (get :n_bar).between?(68,80)
+  
+  cue :phase, 1 if (get :n_bar) == 62
 end
