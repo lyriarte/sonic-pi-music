54f7d4a55afe9cbc8ba12a353ffdc100412d1793
Luc Yriarte <lyriarte@gmail.com>
2022-04-07 23:03:44 +0200

    Comment on the midi keyboard events and global chord management

--- live_kit/midi_keyboard.rb
+++ live_kit/midi_keyboard.rb
@@ -29,15 +29,21 @@ end
 live_loop :midi_note_on do
   use_real_time
   use_synth (get :midi_synth)
+  # sync keydown event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_on"
+  # if lower notes are used as flags in a keymap 
   if (get :play_flags_keymap) and nt < (get :play_flags_keymap).length() then
+    # toggle flag in the keymap indexed by the midi note
     set (get :play_flags_keymap)[nt], (not get (get :play_flags_keymap)[nt])
+  # otherwise add the note to the global midi chord
   else
     ch = (get :midi_chrd).dup
     ch.append(nt)
     set :midi_chrd, ch
+    # if overriding chord progression, set midi chord as current chord
     if get :use_midi_chord
       set :chrd, ch
+    # otherwise play midi chord
     else
       play_midi_chord ch
     end
@@ -46,7 +52,9 @@ end
 
 live_loop :midi_note_off do
   use_real_time
+  # sync keyup event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_off"
+  # remove note from the current midi chord
   ch = (get :midi_chrd).dup
   ch.delete(nt)
   set :midi_chrd, ch
