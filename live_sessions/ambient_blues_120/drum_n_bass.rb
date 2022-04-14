0a662b4a2484c813cf6a82fe1f0880c6940e7899
Luc Yriarte <lyriarte@gmail.com>
2022-04-14 23:34:55 +0200

    blues rock bass

--- live_sessions/ambient_blues_120/drum_n_bass.rb
+++ live_sessions/ambient_blues_120/drum_n_bass.rb
@@ -6,7 +6,11 @@ use_bpm get(:bpm)
 
 live_loop :bass_beat do
   bt, ch = (sync :beat)
+end
 
+live_loop :bass_bar do
+  br, ch = (sync :bar)
+  bass_blues_rock_4 ch, br
 end
 
 live_loop :cymbals_beat do
@@ -24,3 +28,39 @@ end
 
 
 # ---- ---- ---- ---- cymbals and drums
+
+
+# ---- ---- ---- ---- bass
+
+# blues rock bass, 4 beats per bar
+define :bass_blues_rock_4 do | ch, br, am=1, sy=:subpulse |
+  with_synth sy do
+    with_octave -2 do
+      if br % 2 == 0 then
+        3.times do
+          play ch[0], release:0.2, amp: am
+          sleep 0.5
+        end
+        2.times do
+          play ch[0] - 5, release:0.2, amp: am
+          sleep 0.5
+          play ch[0] - 2, release:0.2, amp: am
+          sleep 0.5
+        end
+        play ch[0], release: 1, amp: am
+      else
+        sleep 0.5
+        2.times do
+          play ch[0], release:0.2, amp: am
+          sleep 0.5
+        end
+        2.times do
+          play ch[0] - 5, release:0.2, amp: am
+          sleep 0.5
+          play ch[0] - 2, release:0.2, amp: am
+          sleep 0.5
+        end
+      end
+    end
+  end
+end
