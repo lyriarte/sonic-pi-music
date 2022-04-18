df60ad1ee3b86698f7602e241ded960b8dc56f40
Luc Yriarte <lyriarte@gmail.com>
2022-04-18 15:31:10 +0200

    Bar count play sequence extracted as a function

--- live_sessions/bridge_phases_100_ambient/cues.rb
+++ live_sessions/bridge_phases_100_ambient/cues.rb
@@ -3,8 +3,7 @@
 set :start_beats, nil
 set :use_midi_chord, nil
 
-# uncomment the keymap to toggle play flags with midi keyboard lower notes
-=begin
+# keymap to toggle play flags with midi keyboard lower notes
 set :play_flags_keymap, [
   :play_bass_octave,     # C1
   :play_bass_base,
@@ -19,10 +18,38 @@ set :play_flags_keymap, [
   :play_haunted,
   :play_ambient          # B1
 ]
-=end
 
-# uncomment to clear play flags keymap
-#set :play_flags_keymap, nil
+# uncomment to disable play flags toggle with midi keyboard
+set :play_flags_keymap, nil
+
+# bridge phases ambient 100 bpm default sequence
+define :bridge_phases_ambient_100_sequence do | n_bar |
+  set :play_bass_octave, nil
+  set :play_bass_octave, true if n_bar.between?(16,31)
+  set :play_bass_base, nil
+  set :play_bass_base, true if n_bar < 16 or n_bar >= 64
+  set :play_cymbal_beat, nil
+  set :play_cymbal_beat, true if n_bar < 24
+  set :play_cymbal_sub, nil
+  set :play_cymbal_sub, true if n_bar >= 24
+  set :play_drums_house, nil
+  set :play_drums_house, true if n_bar >= 28
+  set :play_chords_odd, nil
+  set :play_chords_odd, true if n_bar.between?(20,63)
+  set :play_chords_rnd, nil
+  set :play_chords_rnd, true if n_bar.between?(48,63) and one_in 2
+  set :play_chords_beat, nil
+  set :play_chords_beat, true if n_bar < 32
+  set :play_bells_rnd, nil
+  set :play_bells_rnd, true if n_bar.between?(72,95)
+  set :play_chords_rnd_beat, nil
+  set :play_chords_rnd_beat, true if n_bar.between?(16,31)
+  set :play_haunted, nil
+  set :play_haunted, true if n_bar >= 84
+  set :play_ambient, nil
+  set :play_ambient, true if n_bar.between?(68,80)
+  cue :phase, 1 if n_bar == 62
+end
 
 cue :phase, 0
 cue :start
@@ -31,30 +58,6 @@ live_loop :cues do
   br, ch = (sync :bar)
   # toggle play flags according to total bars count if there is no keymap
   if not get :play_flags_keymap then
-    set :play_bass_octave, nil
-    set :play_bass_octave, true if (get :n_bar).between?(16,31)
-    set :play_bass_base, nil
-    set :play_bass_base, true if (get :n_bar) < 16 or (get :n_bar) >= 64
-    set :play_cymbal_beat, nil
-    set :play_cymbal_beat, true if (get :n_bar) < 24
-    set :play_cymbal_sub, nil
-    set :play_cymbal_sub, true if (get :n_bar) >= 24
-    set :play_drums_house, nil
-    set :play_drums_house, true if (get :n_bar) >= 28
-    set :play_chords_odd, nil
-    set :play_chords_odd, true if (get :n_bar).between?(20,63)
-    set :play_chords_rnd, nil
-    set :play_chords_rnd, true if (get :n_bar).between?(48,63) and one_in 2
-    set :play_chords_beat, nil
-    set :play_chords_beat, true if (get :n_bar) < 32
-    set :play_bells_rnd, nil
-    set :play_bells_rnd, true if (get :n_bar).between?(72,95)
-    set :play_chords_rnd_beat, nil
-    set :play_chords_rnd_beat, true if (get :n_bar).between?(16,31)
-    set :play_haunted, nil
-    set :play_haunted, true if (get :n_bar) >= 84
-    set :play_ambient, nil
-    set :play_ambient, true if (get :n_bar).between?(68,80)
-    cue :phase, 1 if (get :n_bar) == 62
+    bridge_phases_ambient_100_sequence (get :n_bar)
   end
 end
