2756e116d103044d1b011bdd42b75b19c9623e1e
Luc Yriarte <lyriarte@gmail.com>
2022-04-18 18:08:54 +0200

    Bridge phases ambient 100 play random mode

--- live_sessions/bridge_phases_100_ambient/cues.rb
+++ live_sessions/bridge_phases_100_ambient/cues.rb
@@ -20,7 +20,14 @@ set :play_flags_keymap, [
 ]
 
 # uncomment to disable play flags toggle with midi keyboard
-set :play_flags_keymap, nil
+#set :play_flags_keymap, nil
+
+# auto play in sequence, random mode, otherwise manual
+set :auto_play_mode, "random" # , "sequence" , nil
+
+# seed the random to the current second
+use_random_seed Time.new.sec
+rand_reset
 
 # bridge phases ambient 100 bpm default sequence
 define :bridge_phases_ambient_100_sequence do | n_bar |
@@ -51,13 +58,66 @@ define :bridge_phases_ambient_100_sequence do | n_bar |
   cue :phase, 1 if n_bar == 62
 end
 
+
+# bridge phases ambient 100 bpm random mode
+define :bridge_phases_ambient_100_random do | br |
+  # bass: change every 16 bars, 80% even among 2
+  if br % 16 == 0 then
+    rnd = rrand(0,1)
+    set :play_bass_octave, nil
+    set :play_bass_base, nil
+    set :play_bass_octave, true if rnd.between?(0,0.4)
+    set :play_bass_base, true if rnd.between?(0.4,0.8)
+  end
+  # cymbal: change every 24 bars, 95% - beat 60% double 35%
+  if br % 24 == 0 then
+    rnd = rrand(0,1)
+    set :play_cymbal_beat, nil
+    set :play_cymbal_sub, nil
+    set :play_cymbal_beat, true if rnd.between?(0,0.6)
+    set :play_cymbal_sub, true if rnd.between?(0.6,0.95)
+  end
+  # drums: change every 4 bars, 80%
+  if br % 4 == 0 then
+    set :play_drums_house, nil
+    set :play_drums_house, true if rrand(0,1) < 0.80
+  end
+  # keyboards change every 8 bars, 80%
+  if br % 8 == 0 then
+    rnd = rrand(0,1)
+    set :play_chords_beat, nil
+    set :play_chords_odd, nil
+    set :play_chords_rnd, nil
+    set :play_chords_rnd_beat, nil
+    set :play_chords_beat, true if rnd.between?(0,0.2)
+    set :play_chords_odd, true if rnd.between?(0.2,0.6)
+    set :play_chords_rnd, true if rnd.between?(0.6,0.75)
+    set :play_chords_rnd_beat, true if rnd.between?(0.75,0.8)
+  end
+  # sounds every 4 bars, 30% with overlap
+  if br % 4 == 0 then
+    rnd = rrand(0,1)
+    set :play_bells_rnd, nil
+    set :play_ambient, nil
+    set :play_haunted, nil
+    set :play_bells_rnd, true if rnd.between?(0.1,0.2)
+    set :play_haunted, true if rnd.between?(0,0.2)
+    set :play_ambient, true if rnd.between?(0.1,0.3)
+  end
+  # low priority cue phase
+  cue :phase, rrand_i(0,1) if rrand_i(0,32) == 0
+end
+
 cue :phase, 0
 cue :start
 
 live_loop :cues do
   br, ch = (sync :bar)
   # toggle play flags according to total bars count if there is no keymap
-  if not get :play_flags_keymap then
+  case get :auto_play_mode
+  when "sequence"
     bridge_phases_ambient_100_sequence (get :n_bar)
+  when "random"
+    bridge_phases_ambient_100_random br
   end
 end
