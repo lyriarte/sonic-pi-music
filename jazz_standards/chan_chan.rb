fec146da5231f90f2af0f7302ab53bda6bef8770
Luc Yriarte <lyriarte@gmail.com>
2022-11-17 22:55:32 +0100

    Chan Chan, even chords on beat 1.5

---
+++ jazz_standards/chan_chan.rb
@@ -0,0 +1,35 @@
+# Chan Chan
+
+set :bpm, 80
+set :beats, 2
+set :sub_beat, 4
+
+set :start_beats, true
+
+set :chords, [
+  (chord :D5, :minor7),  (chord :F5, :major),
+  (chord :G, :minor7),  (chord :A, :dom7),
+  
+  (chord :D5, :minor7),  (chord :F, :major),
+  (chord :G, :minor7),  (chord :A, :dom7)
+  
+].ring
+
+
+# ---- live loops
+
+use_bpm get(:bpm)
+
+live_loop :keys do
+  br, ch = (sync :bar)
+  if br % 2 == 0 then
+    play ch, release: 1
+    sleep 1.5
+    ch = (get :chords)[br+1]
+    play ch, release: 2
+  end
+end
+
+# ---- cues
+
+#cue :start
