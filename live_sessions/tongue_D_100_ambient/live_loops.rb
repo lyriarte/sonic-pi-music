fe7f3e0ec960bc49806dc7214ee85792342c0042
Luc Yriarte <lyriarte@gmail.com>
2024-01-27 23:21:35 +0100

    Ambient D tongue drum impro drum n'bass, chord progression

---
+++ live_sessions/tongue_D_100_ambient/live_loops.rb
@@ -0,0 +1,83 @@
+# ---- ---- ---- ---- keyboards
+
+sync :start
+use_bpm get(:bpm)
+
+live_loop :keys_beat do
+  use_synth :dpulse
+  bt, ch = (sync :beat)
+  keys_chords_odd_sub_beat ch, am=4 if (get :play_chords_odd)
+end
+
+# ---- drum n bass
+
+live_loop :bass_beat do
+  bt, ch = (sync :beat)
+  bass_root_sub ch if (get :play_bass_root)
+  bass_octave_double_beat ch, sb=(get :sub_beat) if (get :play_bass_octave)
+end
+
+live_loop :cymbals_beat do
+  bt, ch = sync :beat
+  sample :drum_cymbal_closed, amp: 0.5 if (get :play_cymbal_beat)
+  drums_cymbals_sub_beat (get :sub_beat) if (get :play_cymbal_sub)
+end
+
+live_loop :drums_beat do
+  bt, ch = sync :beat
+  sample :drum_bass_hard, amp: 0.5 if (get :play_drums_bass)
+  drums_house_odd_snare bt if (get :play_drums_house)
+end
+
+
+# ---- ---- ---- ---- cymbals and drums
+
+# hi hat on sub beat
+define :drums_cymbals_sub_beat do | sb=4, am=1|
+  sample :drum_cymbal_closed, amp: 0.5 * am
+  (sb - 1).times do
+    sleep 1/Float(sb)
+    sample :drum_cymbal_closed, amp: 0.25 * am
+  end
+end
+
+# open hi hat on half beat, snare on 2 and 4
+define :drums_house_odd_snare do | bt, am=1|
+  if (bt % 2) == 0
+    sample :drum_heavy_kick, amp: am
+  else
+    sample :drum_snare_hard, amp: am
+  end
+  sleep 0.5
+  sample :drum_cymbal_pedal, amp: am
+end
+
+
+# ---- ---- ---- ---- bass
+
+# octave 2 notes per beat
+define :bass_octave_double_beat do | ch, am=1, sy=:chipbass |
+  with_synth sy do
+    with_octave -2 do
+      play ch[0], release:0.2, amp: am, pan: -0.8
+      sleep 0.5
+      with_transpose 12 do
+        play ch[0], release:0.2, amp: am, pan: 0.8
+      end
+    end
+  end
+end
+
+# random delay, root chord on first beat
+define :bass_root_sub do | ch, sb=4, am=1, sy=:chipbass |
+  with_synth sy do
+    with_octave -2 do
+      play ch[0], release:0.2, amp: 1.5 * am
+      (sb - 1).times do
+        sleep 1/Float(sb)
+        play ch[0], release:0.2, amp: 1.5 * am
+      end
+    end
+  end
+end
+
--- live_sessions/tongue_D_100_ambient/setup_phases.rb
+++ live_sessions/tongue_D_100_ambient/setup_phases.rb
@@ -12,14 +12,22 @@ set :phases, [
   (chord :Fs, :minor7),  (chord :Fs, :minor7),
 ].ring, 
 [
-  (chord :G, :maj),  (chord :G, :maj)
-].ring
+  (chord :G, :maj),  (chord :A, :dom7),  
+  (chord :B, :minor7),  (chord :A, :dom7), 
+
+  (chord :G, :maj),  (chord :A, :dom7),  
+  (chord :Fs, :minor7),  (chord :Fs, :minor7),
+].ring, 
+[
+  (chord :Fs, :minor7),  (chord :Fs, :minor7),
+  (chord :Fs, :minor7),  (chord :Fs, :minor7),
+].ring, 
 ]
 
 set :chords, (get :phases)[0]
 
 
-set :start_beats, nil
+set :start_beats, true
 set :use_midi_chord, nil
 
 # default midi synths
@@ -31,11 +39,16 @@ set :midi_mod_synth, :mod_tri
 # uncomment to disable play flags toggle with midi keyboard
 set :play_flags_keymap, nil
 
+set :play_chords_odd, true
+
+set :play_bass_root, false
+set :play_bass_octave, false
+
 set :play_cymbal_beat, false
-set :play_bass_base, true
+set :play_cymbal_sub, true
+set :play_drums_bass, true
 set :play_drums_house, false
 
-
-cue :phase, 0
-cue :start
+cue :phase, 1
+#cue :start
 
