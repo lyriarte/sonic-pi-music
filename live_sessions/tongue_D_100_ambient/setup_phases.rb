7035b859e37c9fb7560aeb8f26f4c2722ca32cb7
Luc Yriarte <lyriarte@gmail.com>
2024-02-11 23:39:32 +0100

    movement hash for tongue D ambient

--- live_sessions/tongue_D_100_ambient/setup_phases.rb
+++ live_sessions/tongue_D_100_ambient/setup_phases.rb
@@ -5,22 +5,22 @@ set :beats, 4
 set :sub_beat, 4
 
 set :phases, [
-[
-  (chord :B, :minor7),  (chord :B, :minor7),
-  (chord :B, :minor7),  (chord :B, :minor7),
-  (chord :E, :minor7),  (chord :E, :minor7),
-  (chord :Fs, :minor7),  (chord :Fs, :minor7),
-].ring, 
-[
-  (chord :G, :maj),  (chord :A, :dom7),  
-  (chord :B, :minor7),  (chord :A, :dom7), 
-
-  (chord :G, :maj),  (chord :A, :dom7),  
-  (chord :Fs, :minor7),  (chord :Fs, :minor7),
-].ring, 
-(
-  [ (chord :Fs, :minor7) ] * 8
-).ring, 
+  [
+    (chord :B, :minor7),  (chord :B, :minor7),
+    (chord :B, :minor7),  (chord :B, :minor7),
+    (chord :E, :minor7),  (chord :E, :minor7),
+    (chord :Fs, :minor7),  (chord :Fs, :minor7),
+  ].ring,
+  [
+    (chord :G, :maj),  (chord :A, :dom7),
+    (chord :B, :minor7),  (chord :A, :dom7),
+    
+    (chord :G, :maj),  (chord :A, :dom7),
+    (chord :Fs, :minor7),  (chord :Fs, :minor7),
+  ].ring,
+  (
+    [ (chord :Fs, :minor7) ] * 8
+  ).ring,
 ]
 
 set :chords, (get :phases)[0]
@@ -43,7 +43,7 @@ set :play_flags_keymap, [
   :play_bass_octave,     # C1
   :play_bass_root,
   :play_flag_dummy,      # D1
-  :play_cymbal_beat,     
+  :play_cymbal_beat,
   :play_cymbal_sub,      # E1
   :play_drums_bass,      # F1
   :play_drums_house,
@@ -55,55 +55,16 @@ set :play_flags_keymap, [
 # auto play by movement, in sequence, random mode, otherwise manual
 set :auto_play_mode, "movement" # , "sequence" , "random", nil
 
-# bridge phases ambient 100 bpm random mode
-define :tongue_drum_ambient_100_movements do | movement |
-  for flag in (get :play_flags_keymap) do
-    set flag, false
-  end
-  case movement
-  when "intro"
-    set :play_chords_bar, true
-    set :play_cymbal_beat, true
-    cue :phase, 0
-  when "overture octave"
-    set :play_bass_octave, true
-    set :play_chords_bar, true
-    set :play_cymbal_sub, true
-    cue :phase, 0
-  when "overture double"
-    set :play_bass_root, true
-    set :play_drums_bass, true
-    set :play_chords_odd, true
-    set :play_cymbal_sub, true
-    cue :phase, 0
-  when "overture house"
-    set :play_bass_root, true
-    set :play_drums_house, true
-    set :play_chords_odd, true
-    set :play_cymbal_sub, true
-    cue :phase, 0
-  when "bridge octave"
-    set :play_bass_octave, true
-    set :play_chords_bar, true
-    set :play_cymbal_sub, true
-    set :play_drums_bass, true
-    cue :phase, 1
-  when "bridge house"
-    set :play_bass_octave, true
-    set :play_cymbal_sub, true
-    set :play_drums_house, true
-    cue :phase, 1
-  when "re overture dub"
-    set :play_chords_bar, true
-    set :play_cymbal_beat, true
-    cue :phase, 0
-  when "outro house"
-    set :play_bass_octave, true
-    set :play_chords_bar, true
-    set :play_cymbal_sub, true
-    cue :phase, 2
-  end
-end
+set :movements_map, {
+  "intro" => { "phase" => 0, "flags" => [:play_chords_bar, :play_cymbal_beat]},
+  "overture octave"  => { "phase" => 0, "flags" => [:play_bass_octave, :play_chords_bar, :play_cymbal_sub]},
+  "overture double"  => { "phase" => 0, "flags" => [:play_bass_root, :play_drums_bass, :play_chords_odd, :play_cymbal_sub]},
+  "overture house"  => { "phase" => 0, "flags" => [:play_bass_root, :play_drums_house, :play_chords_odd, :play_cymbal_sub]},
+  "bridge octave"  => { "phase" => 1, "flags" => [:play_bass_octave, :play_chords_bar, :play_cymbal_sub, :play_drums_bass]},
+  "bridge house"  => { "phase" => 1, "flags" => [:play_bass_octave, :play_cymbal_sub, :play_drums_house]},
+  "re overture dub"  => { "phase" => 0, "flags" => [:play_chords_bar, :play_cymbal_beat]},
+  "outro house"  => { "phase" => 2, "flags" => [:play_bass_octave, :play_chords_bar, :play_cymbal_sub]}
+}
 
 live_loop :cues do
   br, ch = (sync :bar)
@@ -112,27 +73,27 @@ live_loop :cues do
   when "movement"
     nb = (get :n_bar)
     if nb < 8
-      tongue_drum_ambient_100_movements "intro"
+      cue :movement, "intro"
     elsif nb < 16
-      tongue_drum_ambient_100_movements "overture octave"
+      cue :movement, "overture octave"
     elsif nb < 24
-      tongue_drum_ambient_100_movements "overture double"
+      cue :movement, "overture double"
     elsif nb < 48
-      tongue_drum_ambient_100_movements "overture house"
+      cue :movement, "overture house"
     elsif nb < 52
-      tongue_drum_ambient_100_movements "bridge octave"
+      cue :movement, "bridge octave"
     elsif nb < 68
-      tongue_drum_ambient_100_movements "bridge house"
+      cue :movement, "bridge house"
     elsif nb < 72
-      tongue_drum_ambient_100_movements "re overture dub"
+      cue :movement, "re overture dub"
     elsif nb < 88
-      tongue_drum_ambient_100_movements "outro house"
-    else 
+      cue :movement, "outro house"
+    else
       set :auto_play_mode, nil
     end
   end
 end
 
-tongue_drum_ambient_100_movements "intro"
 cue :start
+cue :movement, "intro"
 
